465df4bba76cc7da47662e376c9500c5c9c52f6c
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2020-03-13 22:01:59 +0000

    Update Chart.ps1
    This is probably not going to work, but it's late and I figured I'd have a quick go at getting it in before turning in for the night.

--- PowerShell/PSFPLInfo/Tests/Chart.ps1
+++ PowerShell/PSFPLInfo/Tests/Chart.ps1
@@ -1,10 +1,17 @@
-param ([string]$username, [string]$password, [int]$leagueId)
+param ([string]$username, [string]$password, [int]$leagueId, [string]$ssh, [string]$gituser, [string]$gitemail)
 Import-Module -Name $PSScriptRoot\..\PSFPLInfo -Force
 $session = Authenticate $username $password;
 $league = Get-League $leagueId $session;
 $chart = Chart $league;
 $chart.SaveImage("Chart.png", "PNG");
 cd..
+New-Item -ItemType Directory -Force -Path $PSScriptRoot\Output;
+Move-Item -Path $PSScriptRoot\Tests\Chart.png -Destination $PSScriptRoot\Output\Chart.png
 Git add .;
+Git config --local user.email "$gitemail";
+Git config --local user.name "$gituser";
+Git remote set-url origin git@github.com/c-wilkinson/basic-FPL-scrape-scripts;
+start-ssh-agent.cmd -quiet;
+ssh-add "$ssh";
 Git commit -m "Auto commit of new chart";
 Git push origin master;
