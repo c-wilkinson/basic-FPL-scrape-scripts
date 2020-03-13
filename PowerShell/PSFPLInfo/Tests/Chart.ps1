dddd5dd632a7d7ee373ce277170830e105b1fabb
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2020-03-13 21:07:57 +0000

    Update Chart.ps1

--- PowerShell/PSFPLInfo/Tests/Chart.ps1
+++ PowerShell/PSFPLInfo/Tests/Chart.ps1
@@ -4,3 +4,7 @@ $session = Authenticate $username $password;
 $league = Get-League $leagueId $session;
 $chart = Chart $league;
 $chart.SaveImage("Chart.png", "PNG");
+cd..
+Git add .;
+Git commit -m "Auto commit of new chart";
+Git push origin master;
