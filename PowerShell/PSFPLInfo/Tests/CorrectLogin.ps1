592af99d8dce8c357f9225e8b06606d47764dabb
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2020-03-13 22:31:14 +0000

    Create CorrectLogin.ps1

---
+++ PowerShell/PSFPLInfo/Tests/CorrectLogin.ps1
@@ -0,0 +1,18 @@
+param ([string]$username, [string]$password, [int]$leagueId)
+Import-Module -Name $PSScriptRoot\..\PSFPLInfo -Force
+# Note, this uses a correct username and password which is expected to not throw an exception.
+$testState = 0;
+try
+{
+    $test = Authenticate $username $password;
+}
+catch
+{
+    # Exception caught, so we failed to authenticate
+    $testState = 1;
+}
+
+if ($testState -eq 1)
+{
+    throw "FailedTest";
+}
