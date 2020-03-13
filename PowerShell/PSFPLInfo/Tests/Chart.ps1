3da94b47751bbb50e8a2fb99b6023de396b91788
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2020-03-13 20:20:01 +0000

    Update Chart.ps1

--- PowerShell/PSFPLInfo/Tests/Chart.ps1
+++ PowerShell/PSFPLInfo/Tests/Chart.ps1
@@ -1,5 +1,6 @@
 param ([string]$username, [string]$password, [int]$leagueId)
 Install-Module -Name PSFPLInfo
+Import-Module -Name PSFPLInfo
 cd Tests
 $session = Authenticate $username $password;
 $league = Get-League $leagueId $session;
