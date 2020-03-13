5383b10189df016d2bc17a3785ddfdaf42a3c484
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2020-03-13 19:13:56 +0000

    Update Chart.ps1

--- PowerShell/FPL/Tests/Chart.ps1
+++ PowerShell/FPL/Tests/Chart.ps1
@@ -1,5 +1,8 @@
 param ([string]$username, [string]$password, [int]$leagueId)
-
+# This is ugly. . . will fix later
+cd..
+Import-Module .\PSFPLInfo.psm1
+cd Tests
 $session = Authenticate $username $password;
 $league = Get-League $leagueId $session;
 $chart = Chart $league;
