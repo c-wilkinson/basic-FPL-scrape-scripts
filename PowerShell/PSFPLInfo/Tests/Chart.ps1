941227e27dee6c3c8ced854efb49209129b818f8
c-wilkinson <c.a.wilkinson1@googlemail.com>
2020-03-13 22:17:45 +0000

    Update Chart.ps1

--- PowerShell/PSFPLInfo/Tests/Chart.ps1
+++ PowerShell/PSFPLInfo/Tests/Chart.ps1
@@ -4,14 +4,5 @@ $session = Authenticate $username $password;
 $league = Get-League $leagueId $session;
 $chart = Chart $league;
 $chart.SaveImage("Chart.png", "PNG");
-cd..
-New-Item -ItemType Directory -Force -Path $PSScriptRoot\..\Output;
-Move-Item -Path $PSScriptRoot\Chart.png -Destination $PSScriptRoot\..\Output\Chart.png
-#Git add .;
-#Git config --local user.email "$gitemail";
-#Git config --local user.name "$gituser";
-#Git remote set-url origin git@github.com/c-wilkinson/basic-FPL-scrape-scripts;
-#start-ssh-agent.cmd -quiet;
-#ssh-add "$ssh";
-#Git commit -m "Auto commit of new chart";
-#Git push origin master;
+New-Item -ItemType Directory -Force -Path "$PSScriptRoot\..\Output";
+Move-Item -Path "$PSScriptRoot\Chart.png" -Destination "$PSScriptRoot\..\Output\Chart.png";
